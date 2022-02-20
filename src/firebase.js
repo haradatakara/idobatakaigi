1cb29615d66aa23cbe98e5e60ff56c4e90cae11d
haradatakara <h0117031@stu.chs.nihon-u.ac.jp>
2022-02-20 23:24:31 +0900

    remove hard coding

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,30 @@
 import firebase from "firebase";
 
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCl467K1geIV1sWkcMzgFIbCvHi4w2e7-w",
-  authDomain: "idobatakaigi-with-takara.firebaseapp.com",
-  projectId: "idobatakaigi-with-takara",
-  storageBucket: "idobatakaigi-with-takara.appspot.com",
-  messagingSenderId: "435900357040",
-  appId: "1:435900357040:web:6afadfad1acad9562a5e75"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('massages');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = ({ name, text }) => {
-  messageRef.push({name, text})
+  messagesRef.push({name, text})
 }
