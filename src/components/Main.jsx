18f0a4292b12b12e7e35e7dd69f36760fcdb2b1b
haradatakara <h0117031@stu.chs.nihon-u.ac.jp>
2022-02-20 17:28:48 +0900

    create-Massage-MessageInputField

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,24 @@
+import {  makeStyles } from '@material-ui/core/styles'
 import React from "react";
+import Message from './Message';
+import MessageInputField from './MessageInputField';
 
-const Main = () => {
-  return <div>Main</div>;
-  
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateRows: '1fr auto',
+  }
+})
+
+const Main = ({name}) => {
+  const classes = useStyles();
+  return (
+  <div className={classes.root}>
+    <Message />
+    <MessageInputField name={name} />
+  </div>
+  )
 }
 
 export default Main;
---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = () => {
+  return <div>MessageList</div>
+}
+
+export default Message;
