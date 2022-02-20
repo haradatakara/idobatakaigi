924b121d75c1ba6912de8c1bff75e01ca16172eb
haradatakara <h0117031@stu.chs.nihon-u.ac.jp>
2022-02-20 18:58:33 +0900

    apply grid-row

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,
+  }
+})
 const Message = () => {
-  return <div>MessageList</div>
+  const classes = useStyles();
+  return <div className={classes.root}>MessageList</div>
 }
 
 export default Message;
--- src/components/MessageInputField.jsx
+++ src/components/MessageInputField.jsx
@@ -1,7 +1,16 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+
+  }
+})
 const MessageInputField = () => {
-  return <div>MessageInputField</div>
+  const classes = useStyles();
+  return <div className={classes.root}>MessageInputField</div>
 }
 
 export default MessageInputField;
